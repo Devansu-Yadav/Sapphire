1cade655548f5251ff6f3b77621c35b5ae5de0be
Devansu <devansuyadav@gmail.com>
2022-02-12 14:41:56 +0530

    fix: fixed Hamburger menu scroll issue

--- Docs/nav-menu.js
+++ Docs/nav-menu.js
@@ -5,12 +5,12 @@ hamburgerMenuBtn.addEventListener("click", function() {
     if(hamburgerMenuBtn.classList.contains("fa-bars")) {
         hamburgerMenuBtn.classList.remove("fa-bars");
         hamburgerMenuBtn.classList.add("fa-times");
-        document.body.style.overflow = "hidden";
+        // document.body.style.overflow = "hidden";
         navMenu.style.display = "block";
     } else {
         hamburgerMenuBtn.classList.remove("fa-times");
         hamburgerMenuBtn.classList.add("fa-bars");
         navMenu.style.display = "none";
-        document.body.style.overflow = "scroll";
+        // document.body.style.overflow = "scroll";
     }
 })
