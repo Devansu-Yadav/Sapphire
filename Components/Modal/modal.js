60e04cd0243dd3ee92d1cbb65151a8ac432b0fc3
Devansu <devansuyadav@gmail.com>
2022-02-20 21:27:04 +0530

    feat: Added Modal component

---
+++ Components/Modal/modal.js
@@ -0,0 +1,25 @@
+const modalToggleBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".btn-close");
+const modalOverlayBg = document.querySelector(".modal-overlay");
+
+modalToggleBtn.addEventListener("click", () => {
+    if(!modal.classList.contains("show")) {
+        modal.classList.add("show");
+        document.body.style.overflow = "hidden";
+        modalOverlayBg.style.display = "block";
+        modalOverlayBg.style.backgroundColor = "var(--modal-overlay-color)";
+    } else {
+        modal.classList.remove("show");
+        document.body.style.overflow = "auto";
+        modalOverlayBg.style.display = "block";
+        modalOverlayBg.style.backgroundColor = "var(--white-color)";
+    }
+})
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.body.style.overflow = "auto";
+    modalOverlayBg.style.display = "none";
+    modalOverlayBg.style.backgroundColor = "var(--white-color)";
+})
