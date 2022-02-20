5f7812fbbf5bbfc547e40864b9c2e8c9a5931adf
Devansu <devansuyadav@gmail.com>
2022-02-20 21:32:14 +0530

    update: Added code snippet for Modal

--- embed.js
+++ embed.js
@@ -1,4 +1,4 @@
-// JavaScript Code Snippet
+// JavaScript Code Snippet for SnackBar
 const snackbarBaseline = document.querySelector(".snackbar-baseline");
 const snackbarLeading = document.querySelector(".snackbar-leading");
 const snackbarStacked = document.querySelector(".snackbar-stacked");
@@ -29,3 +29,30 @@ const onClickHandler = elem => {
       }, 3000);
     }
 }
+
+// JavaScript Code Snippet for Modal
+const modalToggleBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".btn-close");
+const modalOverlayBg = document.querySelector(".modal-overlay");
+
+modalToggleBtn.addEventListener("click", () => {
+    if(!modal.classList.contains("show")) {
+        modal.classList.add("show");
+        document.body.style.overflow = "hidden";
+        modalOverlayBg.style.display = "block";
+        modalOverlayBg.style.backgroundColor = "var(--modal-overlay-color)";
+    } else {
+        modal.classList.remove("show");
+        document.body.style.overflow = "auto";
+        modalOverlayBg.style.display = "block";
+        modalOverlayBg.style.backgroundColor = "var(--white-color)";
+    }
+})
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.body.style.overflow = "auto";
+    modalOverlayBg.style.display = "none";
+    modalOverlayBg.style.backgroundColor = "var(--white-color)";
+})
