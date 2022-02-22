fa84b30bd33c5f365c5a94e5f9d0f4ea1ffb6a42
Devansu <devansuyadav@gmail.com>
2022-02-22 23:27:50 +0530

    update: Added code snippet for Slider comp

--- embed.js
+++ embed.js
@@ -56,3 +56,19 @@ closeBtn.addEventListener("click", () => {
     modalOverlayBg.style.display = "none";
     modalOverlayBg.style.backgroundColor = "var(--white-color)";
 })
+
+// JavaScript code snippet for Slider component
+const slideValue = document.querySelector(".range-slider-value span");
+const inputSlider = document.querySelector("#range-slider-input");
+
+inputSlider.addEventListener('input', () => {
+    const inputSliderVal = inputSlider.value;
+
+    slideValue.textContent = inputSliderVal;
+    slideValue.style.left = (inputSliderVal/2) + '%';
+    slideValue.classList.add("showSliderVal");
+});
+
+inputSlider.addEventListener('blur', () => {
+    slideValue.classList.remove("showSliderVal");
+});
